fd971f7e54f04d85f982b05aeb4eaac6307688b6
chae1405 <chae1405@nate.com>
2021-08-23 15:05:26 +0900

    cOMMIT

---
+++ memorizegame/app/src/main/java/com/example/memorizegame/MeoryCard.kt
@@ -0,0 +1,5 @@
+package com.example.memorizegame
+
+import android.service.carrier.CarrierIdentifier
+
+data class MeoryCard(val identifier: Int, var isFaceup:Boolean=false, var isMathed:Boolean=false)
